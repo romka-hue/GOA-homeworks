f55b73ea60c517b09c1cf71c0a464aee7ca426da
romka-hue <saxeliucnobia123@gmail.com>
2024-09-18 20:10:11 +0300

    commit

---
+++ day 02/home work.py
@@ -0,0 +1,28 @@
+# დავალების მე-1 საფეხური
+score = 1000
+print("player score is " + str(score))
+
+# დავალების მე-2 საფეხური
+health = 100
+print("player health is " + str(health))
+
+# დავალების მე-3 საფეხური
+attacking_points = 12
+defence_points = 8
+print("the player attacked! score: " + str(score + attacking_points) + " player defenced himself. health: " + str(health + defence_points))
+
+# დავალების მე-4 საფეხური
+english_score = 90
+science_score = 65
+chemistry_score = 70
+history_score = 45
+print("english score: " + str(english_score))
+print("science score: " + str(science_score))
+print("chemistry score: " + str(chemistry_score))
+print("history score: " + str(history_score))
+
+# დავალების მე-5 საფეხური
+name = "Romiko"
+last_name = "Maghradze"
+age = 14
+print("Hi, my name is " + name + " and my last name is " + last_name + " and I'm " + str(age) + " years old.")
---
+++ home work.py
@@ -0,0 +1,38 @@
+#დავალების 1 ნაწილი
+
+name = input( "enter your name : " )
+last_name = input( "enter your last name : ")
+age = input("enter your age : " )
+
+print(name + " " + last_name + " are " + age +" years old!" )
+
+#დავალების მე-2 ნაწილი
+
+first = float(input("place first number that you want to calculate: "))
+second = float(input("place second number that you want to calculate: "))
+
+print(first + second)  # შეკრება
+print(first - second)  # გამოკლება
+print(first / second)  # გაყოფა
+print(first * second)  # გამრავლება
+
+#დავალების მე-3 ნაწილი
+
+user1 = input("enter your name: ")
+print(user1)
+
+#დავალბის მე-4 ნაწილი
+
+math = 20 + 20
+math2 = math - 10
+math3 = math * 2
+print( math3)
+
+#დავალების მე-5 ნაწილი
+
+academy = input("please place your academy's name: ")
+school = input("please place your school's name: ")
+name = input("please place your name: ")
+last_name = input("please place your last name: ")
+
+print("user's academy is " + academy + ", user's school name is " + school + ", and user's name and last name is " + name + " " + last_name)
