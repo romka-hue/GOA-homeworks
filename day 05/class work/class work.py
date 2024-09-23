6bbe6d6c2ebae24d84cdd81bc2943468e56c70ca
romka-hue <saxeliucnobia123@gmail.com>
2024-09-23 20:15:54 +0300

    commit

---
+++ day 05/class work/class work.py
@@ -0,0 +1,12 @@
+name=input("please place your name : ")
+surname=input("pleae place your surname : ")
+
+name1=float(input("place first num :"))
+name2=float(input("place second num :"))
+
+print(name1 - name2)
+print(name1 + name2)
+print(name1 // name2)
+print(name1 / name2)
+print(name1 ** name2)
+print(name1 * name2)
