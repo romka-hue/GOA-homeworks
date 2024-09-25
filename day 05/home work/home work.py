345240e5d4d688b6f70ad9003488baf487581974
romka-hue <saxeliucnobia123@gmail.com>
2024-09-25 21:15:26 +0300

    commit

---
+++ day 05/home work/home work.py
@@ -0,0 +1,29 @@
+
+
+birth_year=int(input("please enter your birth year : "))
+curent_year=2024
+age=curent_year - birth_year
+print("you are " + str(age) ) 
+ 
+ 
+
+
+width=float(input("PLease enter  width of square : "))
+length=float(input("please enter length of square : "))
+area=width*length
+jtcnt=width+length
+perimeter=2*jtcnt
+print("your squares area is : " + str(area))
+print("your squares perimeter is : " + str(perimeter))
+
+
+
+
+km=float(input("please enter length of your home to your school in km : "))
+m=km*1000
+cm=m*100
+mm=cm*10
+print("______________________________________________")
+print(f"{m} metre is between your school and home and there is in cm {cm} and in mm is {mm} ")
+
+
