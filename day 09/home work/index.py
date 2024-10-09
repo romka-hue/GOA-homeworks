62f477295596d74e29d209f529f4f6f6f1bf92e5
romka-hue <saxeliucnobia123@gmail.com>
2024-10-09 21:47:26 +0300

    commit

--- day 09/home work/index.py
+++ day 09/home work/index.py
@@ -1,6 +1,14 @@
 #პიტველი ვერ გავიგე
 temperature_in_c = int(input("please enter you temperature in celcius : "))
 print("YOUR TEMPERATURE IN FRAMHAIT IS : " + str(temperature_in_c * 33.8) +" F" )
+#____________________________
+n = 5 
+
+for i in range(n):  
+   
+    print(' ' * (n - i - 1), end='')
+
+    print('*' * (2 * i + 1))
 
 
 #_______________________
