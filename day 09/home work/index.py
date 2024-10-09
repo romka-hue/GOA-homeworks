2ca1db943bb778c722fad846c0c47c8ddcd2b9f5
romka-hue <saxeliucnobia123@gmail.com>
2024-10-09 21:42:47 +0300

    commit

---
+++ day 09/home work/index.py
@@ -0,0 +1,8 @@
+#პიტველი ვერ გავიგე
+temperature_in_c = int(input("please enter you temperature in celcius : "))
+print("YOUR TEMPERATURE IN FRAMHAIT IS : " + str(temperature_in_c * 33.8) +" F" )
+
+
+#_______________________
+input = int(input("please enter your age : "))
+print(input>20)
