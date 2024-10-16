d3828e0101c377c540a49fb24c352c795e94fe1a
romka-hue <saxeliucnobia123@gmail.com>
2024-10-16 19:50:36 +0300

    commit

---
+++ day 11/home work/home work.py
@@ -0,0 +1,25 @@
+
+for i in range (1, 50, 2):
+    print(i)
+#2
+print("_____________")
+n=0
+while n<4 :
+    print("*"*4)
+    n +=1
+#3
+n=0
+print("_____________")
+while n<4 :
+    print("*"*20)
+    n +=1
+#4
+print("_____________")
+for num1 in range(2):
+    for num in range(2):
+        print(f" num {num} num1 {num1}")
+
+#5 
+#i can't do this task
+
+  
