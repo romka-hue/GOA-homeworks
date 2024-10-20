37d234d35c143e4e430303d8a947210707ec86ed
romka-hue <saxeliucnobia123@gmail.com>
2024-10-20 18:54:50 +0300

    commit

---
+++ day12/home work/hw.py
@@ -0,0 +1,29 @@
+#1
+name = input("Enter your name: ")
+result = ""
+for i in name:
+    result += i + " "
+print(result )
+#2
+
+#3
+
+for j in range(5):  
+    first_num = int(input("Please enter the first number: "))
+    second_num = int(input("Please enter the second number: "))
+    
+    
+    result = first_num + second_num
+    print("The calculated result is: " + str(result)) 
+
+#4 
+
+
+
+for k in range (-100,100):
+    print(k)
+#5 i don't understeand
+#and the boss level
+for i in range(5):
+    print(("*" + "     " + "*")*i)
+
